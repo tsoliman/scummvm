a16e4ff9352e1822efc3a85e486964f715d7a90a
Tarek Soliman <tsoliman@scummvm.org>
2024-05-03 00:40:13 -0500

    SAGA2: Don't check native_mt32

--- engines/saga2/music.cpp
+++ engines/saga2/music.cpp
@@ -46,8 +46,6 @@ Music::Music(hResContext *musicRes) : _musicContext(musicRes), _parser(nullptr) 
 
 	MidiDriver::DeviceHandle dev = MidiDriver::detectDevice(MDT_MIDI | MDT_ADLIB | MDT_PREFER_GM);
 	_driverType = MidiDriver::getMusicType(dev);
-	if (_driverType == MT_GM && ConfMan.getBool("native_mt32"))
-		_driverType = MT_MT32;
 
 	switch (_driverType) {
 	case MT_ADLIB:
